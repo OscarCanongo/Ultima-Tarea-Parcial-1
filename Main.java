1c7293ac35ca6bbc5aa52ef6a717a51777a0e5a7
OscarCanongo <A01730443@itesm.mx>
2020-03-15 13:43:22 -0600

    Mediana

--- Main.java
+++ Main.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 public class Main{
 	public static void main(String[] args) {
 		int semilla=0;
-		double multiplicador=0;
+		int multiplicador=0;
 		int corrimiento=0;
 		int modulo=0;
 		int numeros=0;
-        double aleatorio=0;
+        int aleatorio=0;
 		int cola = 0;
+		int mediana = 0;
 
 		Scanner sc = new Scanner(System.in);
 
@@ -16,17 +17,40 @@ public class Main{
 		corrimiento = sc.nextInt();
 		modulo = sc.nextInt();
 		numeros = sc.nextInt();
-		double[] array = new double[numeros];
+		int[] array = new int[numeros+1];
+		double media = semilla;
 
 		System.out.println(semilla);
+		array[0]=semilla;
 		for (int i=0;i<numeros;i++) {
 			aleatorio=(semilla*multiplicador+corrimiento)%modulo;
         	System.out.println(aleatorio);
             multiplicador=aleatorio;
-			array[i]=aleatorio;	
+			array[i+1]=aleatorio;
+			media+=aleatorio;	
 		}
+		//Revisando si se cumple un ciclo completo
 		if(esPrimo(corrimiento) && esPrimo(modulo)){
 			System.out.println("No hay cola");
+		}
+
+		double mediaImprimir = media/(numeros+1);
+
+		InsertionSort(array);
+
+
+		System.out.print("La media es: ");
+		System.out.printf("%.2f",  mediaImprimir);
+		System.out.println();
+
+		if((numeros+1)%2==0){
+			System.out.println("Las medianas son: ");
+			System.out.println(array[(numeros+1)/2]);
+			System.out.println(((numeros+1)/2)+1);
+		}
+		else{
+			System.out.print("La mediana es ");
+			System.out.println(((numeros+1)/2)+1);
 		}
 	}
 
@@ -64,5 +88,18 @@ public class Main{
 		}
 	}
 
+	public static void InsertionSort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 1; i < n; ++i) { 
+            int key = arr[i]; 
+            int j = i - 1; 
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = key; 
+        } 
+    } 
 	
 }   
